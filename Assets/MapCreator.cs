39b8c7106ec31f41641c0cc69ab94dc82571fc55
anbaut <thibautpenhard@yahoo.fr>
2019-01-23 08:46:17 +0100

    ajout du script et de quelques assets

---
+++ Assets/MapCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _obstacles;
+    private List<int> _map = new List<int>()
+    {
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0
+    };
+
+    void Start()
+    {
+        int size = (int) Math.Sqrt(_map.Count);
+        for (int i = 0; i < _map.Count; ++i)
+        {
+            Instantiate(_obstacles[_map[i]], new Vector3((i % size) * 2, 0, i / size * 2), Quaternion.identity);
+        }
+    }
+}
