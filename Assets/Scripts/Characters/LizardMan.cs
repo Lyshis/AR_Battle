152c2211f14a16bec1a9d1a175cda469cae98ef6
Emma <emma.moussaron@gmail.com>
2019-01-29 17:14:58 +0100

    UI and markers

---
+++ Assets/Scripts/Characters/LizardMan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters {
+	public class LizardMan : MonoBehaviour, ICharacter {
+		[SerializeField] private int movement;
+		[SerializeField] private int initiative;
+		[SerializeField] private int health;
+		[SerializeField] private int defense;
+
+		public int GetMovement() { return movement; }
+		public void DecreaseMovement(int slow) { movement -= slow; }
+		public void IncreaseMovement(int speed) { initiative += speed; }
+		
+		public int GetInitiative() { return initiative; }
+		
+		public int GetHealth() { return health; }
+		public void DecreaseHealth(int damages) { health -= damages; }
+		public void IncreaseHealth(int heal) { health += heal; }
+
+		public int GetDefense() { return defense; }
+		public void DecreaseDefense(int defdown) { defense -= defdown; }
+		public void IncreaseDefense(int defup) { defense += defup; }
+	}
+}
+
---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	[SerializeField] private Button startButton;
+	
+	// Use this for initialization
+	void Start() {
+		startButton.onClick.AddListener(RunGameScene);
+	}
+
+	void RunGameScene() {
+		SceneManager.LoadScene("EmmaScene");
+	}
+}
